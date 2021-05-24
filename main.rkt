6e33eda40c75e8a966c00876b0ad96f8e7ce97a6
Fredrik Meyer <hrmeyer@gmail.com>
2021-05-24 19:10:00 +0200

    Little refacotr

--- main.rkt
+++ main.rkt
@@ -7,6 +7,33 @@
                  (string->number (getenv "PORT"))
                  8080))
 
+;; DATABASE
+
+(define db (make-hasheq))
+
+;; BUSINESS LAYER
+
+(struct todo (title url completed) #:transparent)
+
+(define (todo->dict t)
+  (hash "title" (todo-title t)
+        "url" (todo-url t)
+        "completed" (todo-completed t))
+  )
+
+(define (get-all-todos)
+  (list))
+
+(define (add-new-todo t)
+  t
+  )
+
+(define (delete-all-todos)
+  (list)
+  )
+;; SERVER METHODS
+
+
 (define (make-response s code)
   (response/jsexpr s
                    #:code code
@@ -25,16 +52,26 @@
 
 (define (get-root r)
   (println r)
-  (make-response "Hei" 200))
+  (make-response (get-all-todos) 200))
 
 (define (post-root r)
   (println r)
-  (make-response #hasheq((title . "a todo")) 200))
+  (let* ([post-data (request-post-data/raw r)]
+         [parsed-json (bytes->jsexpr post-data)]
+         [result (add-new-todo (todo "a todo" "" #f))])
+         (make-response (todo->dict result) 200))
+  )
+
+(define (delete-root r)
+  (println r)
+  (make-response (delete-all-todos) 200)
+  )
 
 (define-values (dispatcher dispatcher-url)
   (dispatch-rules
    [("") #:method "get" get-root]
    [("") #:method "post" post-root]
+   [("") #:method "delete" delete-root]
    [else default-response]
    )
   )
