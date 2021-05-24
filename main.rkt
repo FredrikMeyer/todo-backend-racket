98cf8b8fb6147bce9d8110f6c1c570475f4adfe9
Fredrik Meyer <hrmeyer@gmail.com>
2021-05-24 17:09:20 +0200

    Add cookie

--- main.rkt
+++ main.rkt
@@ -6,19 +6,28 @@
 (define port (if (getenv "PORT") (string->number (getenv "PORT"))
                  8080))
 
+
 (define (start req)
   (response/xexpr
    '(html (head (title "Racket Heroku App!!"))
           (body (h1 "It works!")))))
 
+(define (make-response code)
+  (lambda (r) 
+    (response/jsexpr "some reponse"
+                     #:code code
+                     #:headers
+                     (list (header #"access-control-allow-origin" #"*"))))
+  )
+
 (define (not-found r)
-  (response/jsexpr #hasheq((x . 85)) #:code 404)
+  (make-response r 404)
   )
 
 (define-values (go _)
   (dispatch-rules
-   [("catalog") #:method "get" (lambda (r) (response/jsexpr "{}"))]
-   [else not-found]
+   [("catalog") #:method "get" (make-response 200)]
+   [else (make-response 404)]
    )
   )
 
