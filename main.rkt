cf31169e720b9324d2cb0c4ae7f6cb43c50edaae
Fredrik Meyer <hrmeyer@gmail.com>
2021-05-24 16:51:58 +0200

    More than hello world

--- main.rkt
+++ main.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require web-server/servlet
-         web-server/servlet-env)
+         web-server/servlet-env
+         json)
 
 (define port (if (getenv "PORT") (string->number (getenv "PORT"))
                  8080))
@@ -10,8 +11,21 @@
    '(html (head (title "Racket Heroku App!!"))
           (body (h1 "It works!")))))
 
-(serve/servlet start
+(define (not-found r)
+  (response/jsexpr #hasheq((x . 85)) #:code 404)
+  )
+
+(define-values (go _)
+  (dispatch-rules
+   [("catalog") #:method "get" (lambda (r) (response/jsexpr "{}"))]
+   [else not-found]
+   )
+  )
+
+(serve/servlet go
                #:servlet-path "/"
                #:listen-ip #f
                #:command-line? #t
+               #:servlet-regexp #rx""
                #:port port)
+
